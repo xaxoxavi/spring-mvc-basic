d11ad63f7d1a443228b1f98e4e0ca69e481ce897
xaxoxavi <xavi.torrens@gmail.com>
2019-07-02 18:09:35 +0200

    Servlet Init with Java Config. Spring init with java config.

--- src/main/java/com/esliceu/configuration/AnnotationWebAppInitializer.java
+++ src/main/java/com/esliceu/configuration/AnnotationWebAppInitializer.java
@@ -1,20 +1,24 @@
 package com.esliceu.configuration;
 
 import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.support.XmlWebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
-public class XMLWebAppInitializer implements WebApplicationInitializer {
+
+public class AnnotationWebAppInitializer implements WebApplicationInitializer {
 
 
     public void onStartup(ServletContext container) throws ServletException {
         // Punt d'inici de la configuració de Spring
-        XmlWebApplicationContext context = new XmlWebApplicationContext();
-        context.setConfigLocation("/WEB-INF/SpringMVC-servlet.xml");
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(AppConfig.class);
+        context.setServletContext(container);
+
+
 
         ServletRegistration.Dynamic servlet = container.addServlet("dispatcher", new DispatcherServlet(context));
         servlet.setLoadOnStartup(1);
